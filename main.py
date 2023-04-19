30a61b44c6fdd885c8ddcd60951005a529d0b4ff
Andreas Winje <andrebw@stud.ntnu.no>
2023-04-19 12:13:06 +0200

    setup for testing

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from models.fine_tuner import FineTuner
 import datasets
 
 ds = datasets.load_dataset("NicolaiSivesind/human-vs-machine", "wiki_labeled")
-ds_mlt = 0.001
+ds_mlt = 0.1
 ds['train'] = ds['train'].select(range(int(len(ds['train'])*ds_mlt)))
 ds['test'] = ds['test'].select(range(int(len(ds['test'])*ds_mlt)))
 ds['validation'] = ds['validation'].select(range(int(len(ds['validation'])*ds_mlt)))
 
-model_name = "roberta-base-openai-detector"
+model_name = "bigscience/bloomz-560m"
 
 print("="*150)
 print("Initiating trainer...")
-tuner = FineTuner(model_name, ds, num_epochs=1, logging_steps=10, max_tokenized_length=512)
+tuner = FineTuner(model_name, ds, num_epochs=1, logging_steps=100, max_tokenized_length=512)
 print("="*150)
 print("Starting training...")
 tuner.train()
 print("Starting tests...")
+tuner.test()
+print("Starting tests two...")
 tuner.test(ds)
