d4f67ba1f295188287d015e418f778fc6d9553f1
Andreas Winje <andrebw@stud.ntnu.no>
2023-04-20 12:49:30 +0200

    fix: test-method will now take less memory on default dataset

--- models/fine_tuner.py
+++ models/fine_tuner.py
@@ -8,36 +8,16 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trai
 from typing import Dict
 
 
-# Function for computing evaluation metrics
-def compute_metrics(eval_pred):
-    metric1 = evaluate.load("accuracy")
-    metric2 = evaluate.load("precision")
-    metric3 = evaluate.load("recall")
-    metric4 = evaluate.load("f1")
-    
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    accuracy = metric1.compute(predictions=predictions, references=labels)["accuracy"]
-    precision = metric2.compute(predictions=predictions, references=labels)["precision"]
-    recall = metric3.compute(predictions=predictions, references=labels)["recall"]
-    f1 = metric4.compute(predictions=predictions, references=labels)["f1"]
-    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
-
-
-# Fine-tunes a pre-trained model on a specific dataset
 class FineTuner:
+    """Fine-tunes a pre-trained model on a specific dataset"""
     def __init__(self, model_name, dataset, 
                  num_epochs=5, 
-                 num_train_samples=None, 
-                 num_validation_samples=None, 
                  max_tokenized_length=None, 
                  logging_steps=500,
                  ):
         self.dataset = dataset
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2, low_cpu_mem_usage=True)
-        self.num_train_samples = num_train_samples
-        self.num_validation_samples = num_validation_samples
         self.max_tokenized_length = max_tokenized_length
         self.num_epochs = num_epochs
         self.logging_steps = logging_steps
@@ -64,15 +44,10 @@ class FineTuner:
     def init_trainer(self):
         tokenized_datasets = self.dataset.map(self.tokenize_function, batched=True)
 
-        # Training and validation datasets
-        train_dataset = tokenized_datasets["train"].shuffle(seed=42)
-        validation_dataset = tokenized_datasets["validation"].shuffle(seed=42)
-
-        # Select subsets for faster training
-        if (self.num_train_samples):
-            train_dataset = train_dataset.select(range(self.num_train_samples))
-        if (self.num_validation_samples):
-            validation_dataset = validation_dataset.select(range(self.num_validation_samples))
+        # Training, validation and test datasets (tokenized and shuffled) 
+        self.train_dataset = tokenized_datasets["train"].shuffle(seed=42)
+        self.validation_dataset = tokenized_datasets["validation"].shuffle(seed=42)
+        self.test_dataset = tokenized_datasets["test"].shuffle(seed=42)
 
         # E.g "bloomz-560m-wiki_labeled-27000"
         save_name = self.model.config._name_or_path + "-" + self.dataset['train'].config_name + "-" + str(len(self.dataset['train']))
@@ -87,15 +62,14 @@ class FineTuner:
                                           optim='adamw_torch', 
                                           num_train_epochs=self.num_epochs,
                                           auto_find_batch_size=True,
-                                          load_best_model_at_end=True,
         )
         trainer = Trainer(
             model=self.model,
             tokenizer=self.tokenizer,
             args=training_args,
-            train_dataset=train_dataset,
-            eval_dataset=validation_dataset,
-            compute_metrics=compute_metrics,
+            train_dataset=self.train_dataset,
+            eval_dataset=self.validation_dataset,
+            compute_metrics=self.__compute_metrics,
         )
 
         return trainer
@@ -114,12 +88,21 @@ class FineTuner:
                 dict[str, float]:
                     A dictionary containing the metrics
         """
-        # If none provided, use default tuner dataset
-        test_dataset = dataset['test'] if dataset else self.dataset['test']
-        tokenized = test_dataset.map(self.tokenize_function, batched=True)
-        # Prefix for WandB - to destinguish between the tests if running multiple
+
+        # If none provided, use default test-dataset
+        if not dataset:
+            test_dataset = self.test_dataset
+        # If using other dataset, we have to tokenize it
+        else: 
+            # TODO: Seems this is taking waaay too much memory. 
+            # Might need to clear default dataset from memory first somehow. 
+            # More testing is needed though - but testing memory on a cluster is not so much fun.
+            test_dataset = dataset.map(self.tokenize_function, batched=True)["test"]
+
+        # Prefix for WandB - to destinguish between the tests if running multiple tests
         prefix = "test" if not dataset else "test_" + test_dataset.config_name.replace("_","-")
-        metrics = self.trainer.evaluate(tokenized, metric_key_prefix=prefix)
+        metrics = self.trainer.evaluate(eval_dataset=test_dataset, metric_key_prefix=prefix)
+
         # If using default, we still want to log as test/"dataset"_"metric"
         if not dataset:
             prefix = "test/" + test_dataset.config_name.replace("_","-")
@@ -154,3 +137,18 @@ class FineTuner:
             score = probabilities[most_likely_label_index].item()
 
             print(f"Text: {text[:100]}\nPredicted label: {most_likely_label}, Classification Score: {score}\n")
+
+    def __compute_metrics(self, eval_pred):
+        """Function for computing evaluation metrics"""
+        metric1 = evaluate.load("accuracy")
+        metric2 = evaluate.load("precision")
+        metric3 = evaluate.load("recall")
+        metric4 = evaluate.load("f1")
+        
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        accuracy = metric1.compute(predictions=predictions, references=labels)["accuracy"]
+        precision = metric2.compute(predictions=predictions, references=labels)["precision"]
+        recall = metric3.compute(predictions=predictions, references=labels)["recall"]
+        f1 = metric4.compute(predictions=predictions, references=labels)["f1"]
+        return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
