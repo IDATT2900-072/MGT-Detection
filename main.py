8870fb4445a1ba14f811c48989f2b0dab67942db
Andreas Winje <andrebw@stud.ntnu.no>
2023-05-04 11:34:16 +0200

    main will now have ~50 evaluations per epoch; added low_cpu_mem_usage back in fine-tuner; changes in job.slurm

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ model_name = "bigscience/bloomz-560m"
 
 # Parses the job-name from the sbatch script for running bloomz-tuning
 if len(sys.argv) == 2:
+    print("Using arguments from sbatch")
     args = sys.argv[1].split("-")
     if len(args) >= 3:
         model_name = "bigscience/bloomz-" + args[0]
@@ -24,7 +25,7 @@ ds['validation'] = ds['validation'].select(range(int(len(ds['validation'])*ds_ml
 
 print("="*150)
 print("Initiating trainer...")
-tuner = FineTuner(model_name, ds, num_epochs=1, logging_steps=int(500*ds_mlt), max_tokenized_length=512, do_wandb_logging=False)
+tuner = FineTuner(model_name, ds, num_epochs=1, logging_steps=int(max(1, (ds_mlt*len(ds['train'])/(50*8)))), max_tokenized_length=512, do_wandb_logging=True)
 print("="*150)
 print("Starting training...")
 tuner.train()
